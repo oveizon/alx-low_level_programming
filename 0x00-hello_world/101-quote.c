45e537536f25d1828afa6a1c5dc66e7ba8803fca
Wilson <oveizon@gmail.com>
2022-10-14 05:43:48 -0700

    modifications

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -7,6 +7,6 @@ int main(void)
 {
 	char da[11] = "2015-10-19";
 
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, %c \n", da);
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, %s\n", da);
 	return (1);
 }
