05f8c0284befd1140c548f3b33789279e27fbd27
Wilson <oveizon@gmail.com>
2022-10-18 15:21:07 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main - Entry point
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: Always 0
+ */
+/*_putchar - writes the character c to stdout*/
+int _putchar(char c);
+
+int main(void)
+{
+	char print[10] = '_putchar.c';
+
+	_putchar(print);
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,12 @@
+#ifndef putchar
+#define putchar
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+#endif
