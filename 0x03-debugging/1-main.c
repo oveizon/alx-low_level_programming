4b438016bdb567020ffc8da3a3103225386f6aaa
Wilson <oveizon@gmail.com>
2022-10-23 15:40:49 -0700

    corrections

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+
 /**
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -11,7 +13,7 @@ int main(void)
 
 	i = 0;
 
-/*
+/**
 *	while (i < 10)
 *	{
 *		putchar(i);
