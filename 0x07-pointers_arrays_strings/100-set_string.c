eec06297a46539fd2acf5e8170366b86c38fd512
Wilson <oveizon@gmail.com>
2022-10-31 20:11:38 -0700

    initial commit

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - check the code for Holberton School students.
+ * @s : variable
+ * @to : variable
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
