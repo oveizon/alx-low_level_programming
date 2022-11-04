7b5270b3d7e96719f30b5ac5d3f361cbbbba4072
Wilson <oveizon@gmail.com>
2022-11-03 21:54:25 -0700

    initial commit

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#Include "main.h"
+/**
+ * _print_rev_recursion - print revesed string
+ * @s: string to print
+ */
+void _print_rev_recursion(char *s)
+{
+	if (!*s)
+		return;
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
